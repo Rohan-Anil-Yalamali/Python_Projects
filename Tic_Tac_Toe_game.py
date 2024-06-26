f5c217ab43123d23299eff9713efd7df88fa95b5
Rohan Anil Yalamali <ray482002@gmail.com>
2024-06-27 04:31:08 +0530

    Update Tic Tac Toe game

--- Tic_Tac_Toe_game.py
+++ Tic_Tac_Toe_game.py
@@ -15,8 +15,11 @@ def players():
     player1 = 'wrong'
     while player1 not in ['X','O']:
         player1 = input("Player 1 choose your symbol (X or O): ")
+        print('\n')
         if player1 not in ['X','O']:
-            print("sorry, Invalid choice")
+            print('\n')
+            print("Sorry, Invalid choice")
+            print('\n')
     if player1 == "X":
         player2 = 'O'
     else:
@@ -28,21 +31,26 @@ def place_marker(board, marker):
     acceptable_range = range(1,10)
     within_range = False
     already_placed = False
-    
     while position.isdigit() == False or within_range == False or already_placed == False:    
-        position = input("enter your position (1-9): ")
+        position = input("Enter your position (1-9): ")
         if position.isdigit() == False:
-            print("it is not a digit")
+            print('\n')
+            print("It is not a digit")
+            print('\n')
         elif position.isdigit() == True:
             if int(position) in acceptable_range:
                 within_range = True
                 if board[int(position)] == 'X' or board[int(position)] == 'O':
-                    print('Position already taken.Choose another position')
+                    print('\n')
+                    print('Position already taken. Choose another position')
+                    print('\n')
                     already_placed = False
                 else:
                     already_placed = True
             else:
-                print("you are out of the range i.e (0-9)")
+                print('\n')
+                print("You are out of the range i.e. (0-9)")
+                print('\n')
                 within_range = False
     board[int(position)] = marker
     
@@ -55,9 +63,9 @@ def win_check(board, mark):
 def choose_first():
     first = random.randint(1, 2)
     if first == 1:
-        print("player1 will go first")
+        print("Player 1 will go first")
     else:
-        print("player2 will go first")
+        print("Player 2 will go first")
     return first
     
 def full_board_check(board):
@@ -66,15 +74,19 @@ def full_board_check(board):
 def replay():
     user = "wrong"
     while user not in ['Y','y','N','n']:
-        user = input("do you want to play the game again (Y/N): ")
+        user = input("Do you want to play the game again (Y/N): ")
+        print('\n')
         if user not in ['Y','y','N','n']:
-            print("sorry, wrong choice")
+            print('\n')
+            print("Sorry, wrong choice")
+            print('\n')
     if user == "Y" or user == "y":
         return True
     elif user == "N" or user == "n":
         return False
 
 print('Welcome to Tic Tac Toe!')
+print('\n')
 
 game_on = True
 
@@ -82,66 +94,93 @@ while True:
     board = ['0','1','2','3','4','5','6','7','8','9']
     player1,player2 = players()
     print(f'Player1:{player1}\nPlayer2:{player2}')
+    print('\n')
     first = choose_first()
+    print('\n')
     diplay_board(board)
+    print('\n')
     while game_on:
         #player1 turn
         if first == 1:
             print("Player 1 turn")
+            print('\n')
             print(f'Player1:{player1}\nPlayer2:{player2}')
+            print('\n')
             place_marker(board, player1)
+            print('\n')
             diplay_board(board)
+            print('\n')
             if win_check(board, player1):
-                print("congratulations player 1 won the game")
+                print("Congratulations Player 1 won the game")
+                print('\n')
                 break
             else:
                 pass
             if full_board_check(board):
                 print("It's a tie! The board is full. Play once again")
+                print('\n')
                 break
             else:
                 pass
             print("Player 2 turn")
+            print('\n')
             print(f'Player1:{player1}\nPlayer2:{player2}')
+            print('\n')
             place_marker(board, player2)
+            print('\n')
             diplay_board(board)
+            print('\n')
             if win_check(board, player2):
-                print("congratulations player 2 won the game")
+                print("Congratulations Player 2 won the game")
+                print('\n')
                 break
             else:
                 pass
             if full_board_check(board):
                 print("It's a tie! The board is full. Play once again")
+                print('\n')
                 break
             else:
                 pass
         else:
             #player2 turn
             print("Player 2 turn")
+            print('\n')
             print(f'Player1:{player1}\nPlayer2:{player2}')
+            print('\n')
             place_marker(board, player2)
+            print('\n')
             diplay_board(board)
+            print('\n')
             if win_check(board, player2):
-                print("congratulations player 2 won the game")
+                print("Congratulations Player 2 won the game")
+                print('\n')
                 break
             else:
                 pass
             if full_board_check(board):
                 print("It's a tie! The board is full. Play once again")
+                print('\n')
                 break
             else:
                 pass
             print("Player 1 turn")
+            print('\n')
             print(f'Player1:{player1}\nPlayer2:{player2}')
+            print('\n')
             place_marker(board, player1)
+            print('\n')
             diplay_board(board)
+            print('\n')
             if win_check(board, player1):
-                print("congratulations player 1 won the game")
+                print("Congratulations Player 1 won the game")
+                print('\n')
                 break
             else:
                 pass
             if full_board_check(board):
                 print("It's a tie! The board is full. Play once again")
+                print('\n')
                 break
             else:
                 pass
@@ -152,6 +191,7 @@ while True:
     
     
 
+    
     
     
     
