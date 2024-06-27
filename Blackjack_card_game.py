74dec2ee6e2c7030607932f712fe7b1e4ee820d6
Rohan Anil Yalamali <ray482002@gmail.com>
2024-06-27 15:56:50 +0530

    Update Blackjack card game

--- Blackjack_card_game.py
+++ Blackjack_card_game.py
@@ -47,20 +47,10 @@ class Hand:
     def __init__(self):
         self.cards = []  # start with an empty list as we did in the Deck class
         self.value = 0   # start with zero value
-        self.aces = 0    # add an attribute to keep track of aces
     
     def add_card(self,card):
         return self.cards.append(card)
-    
-    def adjust_for_ace(self):
-        ace = 'wrong'
-        while ace not in ['1' , '11']:
-            ace = input('Choose a value for Ace i.e 1 or 11: ')
-            if ace not in ['1', '11']:
-                print("sorry, Invalid choice")
-        self.aces = int(ace)
-        return self.aces
-    
+        
     def __str__(self):
         return f'The hand has {len(self.cards)} cards'
 
@@ -81,29 +71,35 @@ def take_bet():
     while True:
         try:
             ask_bet = int(input("How much are you willing to bet i.e 50 or 100: "))
+            print('\n')
             if chip.total < ask_bet:
-                print("you don't have enough chips")
+                print("You don't have enough chips")
+                print('\n')
                 print(f"Available chips: {chip.total}")
+                print('\n')
                 continue
             elif ask_bet not in [50,100]:
-                print("sorry invalid choice. choose either 50 or 100")
+                print("Sorry invalid choice. choose either 50 or 100")
+                print('\n')
                 continue
         except:
-            print("That is not a number.Please enter a digit")
+            print("That is not a number. Please enter a digit")
+            print('\n')
             continue
         else:
             chip.bet = ask_bet
             print("Thank you for your bet")
+            print('\n')
             break
 
 def player_hit(test_deck, human_player):
     human_player.add_card(test_deck.deal_one())
     if human_player.cards[-1].value == 11:
-        human_player.adjust_for_ace()
-        human_player.cards[-1].value = human_player.aces
-        human_player.value += human_player.cards[-1].value
-    else:
-        human_player.value += human_player.cards[-1].value
+        if human_player.value <= 10:
+            human_player.cards[-1].value = 11
+        else:
+            human_player.cards[-1].value = 1
+    human_player.value += human_player.cards[-1].value
 
 def dealer_hit(test_deck, computer_dealer):
     computer_dealer.add_card(test_deck.deal_one())
@@ -118,8 +114,10 @@ def hit_or_stand(test_deck,human_player):
     global playing  # to control an upcoming while loop
     while playing not in ['hit','stand']:
         playing = input('Do you want to hit or stand: ')
+        print('\n')
         if playing not in ['hit','stand']:
             print("Sorry, Invalid choice")
+            print('\n')
     if playing == 'hit':
         player_hit(test_deck, human_player)
         playing = True
@@ -131,17 +129,21 @@ def show_some(human_player,computer_dealer):
     for show_card in human_player.cards:
         print(f"Player's card: {show_card}")
     print(f"Player's card value: {human_player.value}")
+    print('\n')
     print(f"Dealer's card: {computer_dealer.cards[0]}")
     print("Dealer's one card is hidden")
     print(f"Dealer's card value: {computer_dealer.cards[0].value}")
+    print('\n')
     
 def show_all(human_player,computer_dealer):
     for card_showtime in human_player.cards:
         print(f"Player's card: {card_showtime}")
     print(f"Player's card value: {human_player.value}")
+    print('\n')
     for card_show_time in computer_dealer.cards:
         print(f"Dealer's card: {card_show_time}")
     print(f"Dealer's card value: {computer_dealer.value}")
+    print('\n')
     
 def player_busts():
     if human_player.value > 21:
@@ -176,9 +178,11 @@ def tie():
 def replay():
     user = "wrong"
     while user not in ['Y','y','N','n']:
-        user = input("do you want to play the game again (Y/N): ")
+        user = input("Do you want to play the game again (Y/N): ")
+        print('\n')
         if user not in ['Y','y','N','n']:
-            print("sorry, wrong choice")
+            print("Sorry, wrong choice")
+            print('\n')
     if user == "Y" or user == "y":
         return True
     elif user == "N" or user == "n":
@@ -186,6 +190,7 @@ def replay():
     
 while True:
     print("Welcome to Blackjack card game")
+    print('\n')
 
     # Create & shuffle the deck, deal two cards to each player
     test_deck = Deck()
@@ -196,13 +201,12 @@ while True:
     
     # Inform Player of their chips total 
     print(f"Available chips: {chip.total}")
+    print('\n')
     
     # Prompt the Player for their bet
     take_bet()
     
     player_hit(test_deck, human_player)
-    print(f"Player's first card: {human_player.cards[0]}")
-    print(f"Player's first card value: {human_player.cards[0].value}")
     player_hit(test_deck, human_player)
     
     dealer_hit(test_deck, computer_dealer)
@@ -211,7 +215,7 @@ while True:
     # Show cards (but keep one dealer card hidden)
     show_some(human_player, computer_dealer)
     
-    while playing:  # recall this variable from our hit_or_stand function
+    while playing:  # recall this variable from the hit_or_stand function
         
         # Prompt for Player to Hit or Stand
         hit_or_stand(test_deck, human_player)
@@ -229,7 +233,9 @@ while True:
         chip.lose_bet()
         print("Player got busted")
         print(f"Player's card value: {human_player.value}")
+        print('\n')
         print("Dealer won")
+        print('\n')
     else:
         pass
     
@@ -244,32 +250,41 @@ while True:
         if tie():
             chip.win_bet()
             print("It's a tie")
+            print('\n')
         
         elif dealer_busts():
             chip.win_bet()
             print("Dealer got busted")
             print(f"Dealer's card value: {computer_dealer.value}")
+            print('\n')
             print("Player won")
             print(f"Player's card value: {human_player.value}")
+            print('\n')
 
         elif player_wins():
             chip.win_bet()
             print("Player won")
+            print('\n')
             print(f"Player's card value: {human_player.value}")
             print(f"Dealer's card value: {computer_dealer.value}")
+            print('\n')
 
         elif dealer_wins():
             chip.lose_bet()
             print("Dealer won")
+            print('\n')
             print(f"Dealer's card value: {computer_dealer.value}")
             print(f"Player's card value: {human_player.value}")
+            print('\n')
 
     # Inform Player of their chips total 
     print(f"Available chips: {chip.total}")
+    print('\n')
 
     #If player doesn't have minimum chips to bet, break out of the loop
     if chip.total == 0:
         print("Sorry, you don't have enough chips to play another round")
+        print('\n')
         break
     
     # Ask to play again
